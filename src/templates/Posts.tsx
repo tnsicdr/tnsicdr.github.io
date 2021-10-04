b9e7d6cb21bdb538240adc6b285f7c3fabeac04d
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2021-10-04 02:52:20 -0400

    Update Pagination Control

--- src/templates/Posts.tsx
+++ src/templates/Posts.tsx
@@ -1,8 +1,16 @@
 import { graphql } from 'gatsby';
 import parse from 'html-react-parser';
+import tw, { styled } from 'twin.macro';
 import { GatsbyLayout } from '../components/Layout/GatsbyLayout';
 import { Link } from '../components/Link/link';
 import { PostExcerpt } from '../components/PostExcerpt/PostExcerpt';
+
+const Wrapper = styled.div`
+    .pagination-controls {
+        ${tw`flex flex-row justify-between prose`}
+        max-width: 100%;
+    }
+`;
 
 const PostsTemplate = ({
     data,
@@ -15,43 +23,45 @@ const PostsTemplate = ({
 
     return (
         <GatsbyLayout>
-            {posts.map((post, index) => {
-                const title = String(post.title);
-                const date = new Date(post.date);
-                const slug = `/post/${String(post.slug)}`;
+            <Wrapper>
+                {posts.map((post, index) => {
+                    const title = String(post.title);
+                    const date = new Date(post.date);
+                    const slug = `/post/${String(post.slug)}`;
 
-                const excerptContent = parse(post.excerpt);
-                let content;
+                    const excerptContent = parse(post.excerpt);
+                    let content;
 
-                // For the excerpt, only the text content matters
-                if (Array.isArray(excerptContent)) {
-                    content = excerptContent[0];
-                    if (Array.isArray(content.props.children)) {
-                        content.props.children.splice(
-                            1,
-                            content.props.children.length
-                        );
+                    // For the excerpt, only the text content matters
+                    if (Array.isArray(excerptContent)) {
+                        content = excerptContent[0];
+                        if (Array.isArray(content.props.children)) {
+                            content.props.children.splice(
+                                1,
+                                content.props.children.length
+                            );
+                        }
+                    } else {
+                        content = excerptContent;
                     }
-                } else {
-                    content = excerptContent;
-                }
 
-                return (
-                    <PostExcerpt
-                        key={index}
-                        slug={slug}
-                        title={title}
-                        date={date}
-                        body={content}
-                    />
-                );
-            })}
-            <div>
-                {previousPagePath && (
-                    <Link to={previousPagePath}>Previous Page</Link>
-                )}
-                {nextPagePath && <Link to={nextPagePath}>Next Page</Link>}
-            </div>
+                    return (
+                        <PostExcerpt
+                            key={index}
+                            slug={slug}
+                            title={title}
+                            date={date}
+                            body={content}
+                        />
+                    );
+                })}
+                <div className="pagination-controls">
+                    {previousPagePath && (
+                        <Link to={previousPagePath}>Previous Page</Link>
+                    )}
+                    {nextPagePath && <Link to={nextPagePath}>Next Page</Link>}
+                </div>
+            </Wrapper>
         </GatsbyLayout>
     );
 };
