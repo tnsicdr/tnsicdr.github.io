a082ccd7d99c2f1aa0da02abbcf397ac4b4f78d8
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2021-12-08 03:11:58 -0500

    Updates

--- component---src-templates-page-tsx-52ad2a1ce250d4aa6b62.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktnsicdr_github_io=self.webpackChunktnsicdr_github_io||[]).push([[106],{8610:function(t,n,u){u.r(n),u.d(n,{default:function(){return a}});u(7294);var i=u(3310),e=u(6811),r=u(3431),l=function(t){t.title;var n=t.body;return(0,r.tZ)("div",null,(0,r.tZ)(e.Z,null,n))},c=u(1910),a=function(t){var n=t.data.page,u=String(n.title),e=(0,i.ZP)(n.content);return(0,r.tZ)(c.z,null,(0,r.tZ)(l,{title:u,body:e}))}}}]);
-//# sourceMappingURL=component---src-templates-page-tsx-52ad2a1ce250d4aa6b62.js.map
