53a461640d9ed399fa6c875bcd91d736067906ed
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2022-04-10 11:17:53 -0400

    feat: Add archive listing

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,6 +1,11 @@
+import { faCalendar, faTags } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { graphql, PageProps } from 'gatsby';
 import Layout from '../components/Layout/Layout';
+import Link from '../components/Link/link';
 import Seo from '../components/Seo/Seo';
+import TagList from '../components/TagList/TagList';
+import { resolvePostUrl } from '../utilities/resolvers/post-url-resolver';
 
 type DataProps = {
   site: {
@@ -8,13 +13,49 @@ type DataProps = {
       title: string;
     };
   };
+  allMdx: {
+    nodes: Array<{
+      frontmatter: {
+        title: string;
+        date: Date;
+        slug: string;
+        tags: Array<string>;
+      };
+      excerpt: string;
+    }>;
+  };
 };
 
 const PostsPage = ({ data }: PageProps<DataProps>) => {
+  const posts = data.allMdx.nodes;
+  
   return (
     <>
       <Seo title="archive" />
       <Layout siteTitle={data.site.siteMetadata.title}>
+        <ul className="list-none space-y-2">
+          {posts.map(post => (
+            <li key={post.frontmatter.slug}>
+              <article>
+                <Link to={resolvePostUrl('/posts', post.frontmatter)}>
+                  <h2 className="font-medium text-2xl hover:text-slate-400">
+                    {post.frontmatter.title}
+                  </h2>
+                </Link>
+                <div className="meta mb-1 flex flex-col md:flex-row gap-x-4">
+                  <div className="meta-date">
+                    <FontAwesomeIcon icon={faCalendar} className="mr-1" />
+                    {post.frontmatter.date}
+                  </div>
+                  <div className="meta-tags">
+                    <FontAwesomeIcon icon={faTags} className="mr-1" />
+                    <TagList tags={post.frontmatter.tags} />
+                  </div>
+                </div>
+              </article>
+            </li>
+          ))}
+        </ul>
       </Layout>
     </>
   );
@@ -26,6 +67,21 @@ export const query = graphql`
   query PostsQuery {
     site {
       ...SiteMetadata
+    }
+    allMdx(
+      filter: { frontmatter: { type: { eq: "post" } } }
+      sort: { fields: frontmatter___date, order: DESC }
+    ) {
+      nodes {
+        frontmatter {
+          title
+          date
+          slug
+          tags
+        }
+        excerpt(pruneLength: 500)
+        id
+      }
     }
   }
 `;
