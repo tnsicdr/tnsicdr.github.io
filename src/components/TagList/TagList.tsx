ce02bd8adbb740bde8e74820cebbc5fc41083d5f
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2022-04-09 22:55:44 -0400

    fix: TagList key on the outermost component

--- src/components/TagList/TagList.tsx
+++ src/components/TagList/TagList.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import { resolveTagUrl } from '../../utilities/resolvers/tag-url-resolver';
 import Link from '../Link/link';
 
@@ -11,12 +12,12 @@ const TagList = (props: TagListProps) => {
   return (
     <span>
       {tags.map((tag, index) => (
-        <>
-          <Link key={tag} to={resolveTagUrl(tag)} className="hover:underline">
+        <Fragment key={tag}>
+          <Link to={resolveTagUrl(tag)} className="hover:underline">
             {tag}
           </Link>
           {index !== tags.length - 1 && <span>, </span>}
-        </>
+        </Fragment>
       ))}
     </span>
   );
