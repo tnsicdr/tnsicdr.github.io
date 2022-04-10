8ad866540dcfacee4a123388e62adc283f1536db
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2022-04-10 03:11:54 -0400

    fix: Fix test failing due to static query

--- src/components/Layout/layout.test.tsx
+++ src/components/Layout/layout.test.tsx
@@ -1,6 +1,20 @@
 import renderer from 'react-test-renderer';
 import Layout from './Layout';
+import * as Gatsby from 'gatsby';
 
+const useStaticQuery = jest.spyOn(Gatsby, 'useStaticQuery');
+useStaticQuery.mockImplementation(() => ({
+  site: {
+    siteMetadata: {
+      title: 'title',
+      description: 'description',
+      social: {
+        twitter: 'twitter',
+        github: 'github',
+      }
+    },
+  },
+}));
 
 describe('<Layout />', () => {
   test('renders', () => {
