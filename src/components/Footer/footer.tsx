f48a269cdd9ecce2a0d2ea3f047a92547374629c
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2022-04-09 21:35:38 -0400

    feat: Add social links

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,22 +1,67 @@
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import clsx from 'clsx';
+import { graphql, useStaticQuery } from 'gatsby';
 import Link from '../Link/link';
 
-interface IFooterProps {
+type FooterProps = {
   className?: string;
-}
+};
 
-const Footer = (props: IFooterProps) => {
+const Footer = (props: FooterProps) => {
   const { className } = props;
+
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            social {
+              twitter
+              github
+            }
+          }
+        }
+      }
+    `
+  );
+
+  const twitter = site.siteMetadata?.social?.twitter;
+  const github = site.siteMetadata?.social?.github;
+
   return (
     <footer className={clsx('bg-gray-300', className)}>
-      <div className="flex flex-row justify-end px-8 py-4">
+      <div className="flex flex-col md:flex-row md:justify-between px-8 py-4">
+        <div className="flex flex-col text-sm">
+          <div>
+            <ul className="list-none flex flex-row gap-2 text-lg">
+              {twitter && (
+                <li>
+                  <Link to={`https://twitter.com/${twitter}`}>
+                    <FontAwesomeIcon icon={faTwitter} aria-label="twitter" />
+                  </Link>
+                </li>
+              )}
+              {github && (
+                <li>
+                  <Link to={`https://github.com/${github}`}>
+                    <FontAwesomeIcon icon={faGithub} aria-label="twitter" />
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
         <div className="flex flex-col text-sm">
           <span className="self-end">
             copyright &copy; {new Date().getFullYear()}
           </span>
           <span className="self-end">
             built with{' '}
-            <Link className="underline hover:text-gray-500" to="https://www.gatsbyjs.com/">
+            <Link
+              className="underline hover:text-gray-500"
+              to="https://www.gatsbyjs.com/"
+            >
               gatsbyjs
             </Link>
           </span>
