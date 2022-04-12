3932dd14cdc51e00ae27e85c72511c7fd15ef4f8
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2022-04-12 02:37:11 -0400

    fix: Add python coloring

--- jest-preprocess.js
+++ jest-preprocess.js
@@ -24,6 +24,7 @@ const babelOptions = {
                     'swift',
                     'ruby',
                     'scss',
+                    'python',
                 ],
                 plugins: [
                     'show-language',
