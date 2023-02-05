814bc5ce874280c13f243178ab1e6fb8e7c7cc0d
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2023-02-05 15:00:06 -0500

    feat: Add RSS

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,35 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
+import remarkHtml from "remark-html";
+import sanitizeHtml from "sanitize-html";
+
+export async function get(context: any) {
+  const publishedBlogPosts = await getCollection(
+    "blog",
+    ({ data }) => !data.draft
+  );
+  return rss({
+    title: "code.tnsi",
+    description: "",
+    site: context.site,
+    items: publishedBlogPosts.map((post) => {
+      const parsedMarkdown = unified()
+        .use(remarkParse)
+        .use(remarkGfm)
+        .use(remarkHtml)
+        .processSync(post.body);
+
+      const htmlContent = sanitizeHtml(String(parsedMarkdown));
+
+      return {
+        title: post.data.title,
+        pubDate: post.data.date,
+        link: `/post/${post.slug}`,
+        content: htmlContent,
+      };
+    }),
+  });
+}
